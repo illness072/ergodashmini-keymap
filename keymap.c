d6d1173e9f3e53bb3b3af6af276ca09d2ac30f13
illness072 <55215+illness072@users.noreply.github.com>
2019-08-02 11:00:56 +0900

    defined shortern name

--- keymap.c
+++ keymap.c
@@ -15,6 +15,10 @@ enum custom_keycodes {
 };
 
 #define EISU LALT(KC_GRV)
+#define KCX_PW KC_POWER
+#define KCX_VU KC__VOLUP
+#define KCX_VD KC__VOLDOWN
+#define KCX_MU KC__MUTE
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -33,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_MINS,                        KC_EQL , KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,                        KC_RBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_BSLS,                        KC_GRV , KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_GRV , \
-    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,        KC_SPC, KC_ENT,  KC_BSPC, RAISE,            XXXXXXX, KC_MINS, KC_EQL   \
+    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,       KC_SPC,  KC_ENT,  KC_BSPC, RAISE,            XXXXXXX, KC_MINS, KC_EQL   \
   ),
 
   /* Lower
@@ -51,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_UNDS,                        KC_PLUS, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_PIPE,  \
     KC_LCTL, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_LCBR,                        KC_RCBR, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DQT,  \
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,                        KC_TILD, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_TILD, \
-    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,        KC_SPC, KC_ENT,  KC_DEL,  RAISE,            XXXXXXX, KC_UNDS, KC_PLUS  \
+    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,       KC_SPC,  KC_ENT,  KC_DEL,  RAISE,            XXXXXXX, KC_UNDS, KC_PLUS  \
   ),
 
   /* Raise
@@ -69,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_UNDS,                        KC_PLUS, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_PIPE,  \
     KC_LCTL, KC_F11 , KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, KC_LCBR,                        KC_RCBR, KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, XXXXXXX, KC_DQT,  \
     KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_PIPE,                        KC_TILD, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  XXXXXXX, KC_TILD, \
-    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,        KC_SPC, KC_ENT,  KC_DEL,  RAISE,            XXXXXXX, KC_UNDS, KC_PLUS  \
+    KC_ESC,  XXXXXXX, KC_LALT,          LOWER,   KC_LCMD, KC_LSFT, KC_SPC,       KC_SPC,  KC_ENT,  KC_DEL,  RAISE,            XXXXXXX, KC_UNDS, KC_PLUS  \
     ),
 
   /* Adjust
@@ -84,10 +88,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * ,----------------------------------------------------------------------------------------------------------------------.
    */
   [_ADJUST] = LAYOUT(
-    KC_POWER,XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX,KC__VOLDOWN,KC__VOLUP,KC__MUTE,XXXXXXX,XXXXXXX,XXXXXXX,                       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, KC_BRMD, KC_BRMU, XXXXXXX, XXXXXXX, XXXXXXX,XXXXXXX,                         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
-    XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX,XXXXXXX,XXXXXXX,         XXXXXXX,XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX  \
+    KCX_PW,  XXXXXXX, KC_PSCR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, KCX_VD,  KCX_VU,  KCX_MU,  XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, KC_BRMD, KC_BRMU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
+    XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,          XXXXXXX, XXXXXXX, XXXXXXX  \
   )
 };
 
